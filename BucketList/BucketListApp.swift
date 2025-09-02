d04f740dec4efeae0162c0250889902136f1bd21
Deser7 <deser7@icloud.com>
2025-09-03 00:47:02 +0400

    реализовал добавление местоположения

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Наташа Спиридонова on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Наташа Спиридонова on 03.09.2025.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    
+    @State private var locations: [Location] = []
+    
+    var body: some View {
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(
+                        name: "New location",
+                        description: "",
+                        latitude: coordinate.latitude,
+                        longitude: coordinate.longitude
+                    )
+                    locations.append(newLocation)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
